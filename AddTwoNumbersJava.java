591e28e0457565566c751867372294cd152fe4fb
Bharath-Aru <bharatharu771@gmail.com>
2024-01-15 08:16:19 +0530

    Java Files added

---
+++ AddTwoNumbersJava.java
@@ -0,0 +1,15 @@
+public class AddTwoNumbersJava{
+    public static void main(String[] args) {
+        // Specify the two numbers
+        double num1 = 26;
+        double num2 = 5;
+
+        // Calculate the sum of the two numbers
+        double sum = num1 + num2;
+
+        // Display the result
+        System.out.println("This is Bharath.A 20MIC0055");
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+    }
+}
